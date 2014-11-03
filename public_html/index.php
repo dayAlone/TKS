122a1ec3e37707fdcd07a327b7c2d6483fffb6b5
dayAlone <andrey.slider@gmail.com>
2014-11-04 04:18:53 +0500

    Slider

--- public_html/index.php
+++ public_html/index.php
@@ -17,43 +17,5 @@ $APPLICATION->IncludeComponent(
   ),
   false
 );
-$APPLICATION->IncludeComponent(
-  "bitrix:news.list", 
-  "news_index",
-  array(
-    "IBLOCK_ID"   => 2,
-    "NEWS_COUNT"  => "1",
-    "SORT_BY1"    => "ACTIVE_FROM",
-    "SORT_ORDER1" => "DESC",
-    "DETAIL_URL"  => "/press/#ELEMENT_CODE#/",
-    "CACHE_TYPE"  => "A",
-    "SET_TITLE"   => "N",
-  ),
-  false
-);
-$APPLICATION->IncludeComponent("bitrix:catalog.section.list", "sections_index", array(
-    "IBLOCK_TYPE"  => "news",
-    "IBLOCK_ID"    => "1",
-    "TOP_DEPTH"    => "1",
-    "CACHE_TYPE"   => "A",
-    "SECTION_USER_FIELDS" => array("UF_SVG")
-),
-false
-);
-$APPLICATION->IncludeComponent(
-  "bitrix:news.list", 
-  "partners_index",
-  array(
-    "IBLOCK_ID"   => 4,
-    "NEWS_COUNT"  => "10",
-    "SORT_BY1"    => "SORT",
-    "SORT_ORDER1" => "ASC",
-    "DETAIL_URL"  => "/",
-    "CACHE_TYPE"  => "A",
-    "SET_TITLE"   => "N",
-    "PROPERTY_CODE" => Array("LINK"),
-  ),
-  false
-);
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
--- public_html/local/templates/.default/components/bitrix/news.list/slider/template.php
+++ public_html/local/templates/.default/components/bitrix/news.list/slider/template.php
@@ -1,4 +1,4 @@
-<div class="slider fotorama" data-width="100%" data-height="80%" data-minheight="640" data-nav="false" data-autoplay="true" data-transition="crossfade" data-loop="true">
+<div class="slider fotorama" data-width="100%" data-height="100%" data-minheight="640" data-nav="false" data-autoplay="true" data-transition="crossfade" data-loop="true">
 	<?foreach ($arResult['ITEMS'] as $item):?>
 		<div style="background-image: url(<?=$item['PREVIEW_PICTURE']['SRC']?>)" class="slider__item slider__item--<?=$item['PROPERTIES']['POSITION']['VALUE_XML_ID']?>">
 		  <div class="container">

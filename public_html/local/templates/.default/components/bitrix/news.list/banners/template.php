b5ec4c3bf55376a1e5061cea28cee980b1d1aead
dayAlone <andrey.slider@gmail.com>
2014-11-04 20:12:45 +0500

    Changes

--- public_html/local/templates/.default/components/bitrix/news.list/banners/template.php
+++ public_html/local/templates/.default/components/bitrix/news.list/banners/template.php
@@ -1,18 +1,19 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 $this->setFrameMode(true);
-if(count($arResult['SECTIONS'])>0):
+if(count($arResult['ITEMS'])>0):
 ?>
 <div class="sections-side">
-	<?foreach ($arResult['ITEMS'] as $key => &$item):
+	<?
+	foreach ($arResult['ITEMS'] as $key => &$item):
     ?>
-	<a href="<?=$item['SECTION_PAGE_URL']?>" class="sections-side__item" style="background-image: url(<?=$item['PPREVIEW_PICTURE']['SRC']?>)">
+	<a href="<?=$item['SECTION_PAGE_URL']?>" class="sections-side__item" style="background-image: url(<?=$item['PREVIEW_PICTURE']['SRC']?>)">
         <div class="sections-side__frame">
     		<div class="sections-side__icon">
-    			<?=file_get_contents($_SERVER['DOCUMENT_ROOT'].CFile::GetPath($item['PROPERTIES']['SVG']))?>
+    			<?=file_get_contents($_SERVER['DOCUMENT_ROOT'].CFile::GetPath($item['PROPERTIES']['SVG']['VALUE']))?>
     		</div>
     		<div class="sections-side__text">
                 <span class="sections-side__title"><?=$item['NAME']?></span><br>
-                <span class="sections-side__description"><?=$item['PROPERTIES']['DESCRIPTION']?></span>
+                <span class="sections-side__description"><?=$item['PROPERTIES']['DESCRIPTION']['VALUE']?></span>
             </div>
         </div>
 	</a>

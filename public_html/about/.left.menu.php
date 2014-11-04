3b51eb44c79a3c6a8df9a5ceac55bdce22fc2433
dayAlone <andrey.slider@gmail.com>
2014-11-05 03:21:31 +0500

    Nav

--- public_html/about/.left.menu.php
+++ public_html/about/.left.menu.php
@@ -7,13 +7,14 @@ $aMenuLinks = Array(
 		Array(), 
 		"" 
 	),
+	/*
 	Array(
 		"История", 
 		"/about/history/", 
 		Array(), 
 		Array(), 
 		"" 
-	),
+	),*/
 	Array(
 		"Миссия", 
 		"/about/mission/", 
@@ -27,13 +28,14 @@ $aMenuLinks = Array(
 		Array(), 
 		Array(), 
 		"" 
-	),
+	)
+	/*,
 	Array(
 		"Партнеры", 
 		"/about/partners/", 
 		Array(), 
 		Array(), 
 		"" 
-	)
+	)*/
 );
 ?>

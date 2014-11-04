c461202569f53bb4738837c8e8b39ab126bfd846
dayAlone <andrey.slider@gmail.com>
2014-11-05 02:18:29 +0500

    Changes

---
+++ public_html/structure/index.php
@@ -0,0 +1,10 @@
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetTitle('Структура');?>
+<div class="page__content">
+	<h2>В промышленный холдинг ТКС входят 20 компаний, осуществляющих свою деятельность в России и за рубежом в различных областях науки и промылшенности. </h2>
+	<img src="/layout/images/structure.jpg">
+</div>
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+?>

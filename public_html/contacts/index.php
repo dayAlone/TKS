b83aeb537f64c89066bdb23fc9a8a1d73fdb10e8
dayAlone <andrey.slider@gmail.com>
2014-11-04 21:11:10 +0500

    CSS

--- public_html/contacts/index.php
+++ public_html/contacts/index.php
@@ -6,7 +6,7 @@ $APPLICATION->SetPageProperty('body_class', "contacts");?>
 	<h2>Головной офис</h2>
 	<div class="row">
 	  <div class="col-sm-8">
-	    <big><strong>Адрес: </strong>Россия, Москва, <nobr>ул. Усачева, 35А</nobr> <br><strong>Телефон: </strong><a href="tel:88005553136 ">8 800 555 31 36</a><br><a href="tel:84956265494" style="margin-left:95px;">8 495 626 54 94</a><br><strong>E-mail: </strong><a href="mailto:info@tkc-group.ru">info@tkc-group.ru</a></big><br>
+	    <big><strong>Адрес: </strong>Россия, Москва, <nobr>ул. Усачева, 35А</nobr> <br><strong>Телефон: </strong><a href="tel:88005553136 ">8 800 555 31 36</a><br><a href="tel:84956265494" style="margin-left:93px;">8 495 626 54 94</a><br><strong>E-mail: </strong><a href="mailto:info@tkc-group.ru">info@tkc-group.ru</a></big><br>
 	  </div>
 	  <div class="col-sm-4 right">
 	    <div class="xs-center md-right"><a data-toggle="modal" data-target="#Feedback" href="#Feedback" class="no-margin-top button button--big">напишите нам</a></div>

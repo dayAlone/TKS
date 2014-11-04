e0ee2b736fa6b1c025189d9ffe111114dd454693
dayAlone <andrey.slider@gmail.com>
2014-11-05 01:14:44 +0500

    Changes

--- public_html/local/templates/.default/components/bitrix/news.detail/industries/template.php
+++ public_html/local/templates/.default/components/bitrix/news.detail/industries/template.php
@@ -11,7 +11,7 @@ $props = &$item["PROPS"];
 	    "IBLOCK_ID"    => "7",
 	    "TOP_DEPTH"    => "1",
 	    "CACHE_TYPE"   => "A",
-	    "CACHE_NOTES"  => $_REQUEST['ELEMENT_CODE'],
+	    "CACHE_NOTES"  => $arResult['CODE'],
 	    "SECTION_USER_FIELDS" => array("UF_SVG", "UF_TEXT")
 	),
 	false

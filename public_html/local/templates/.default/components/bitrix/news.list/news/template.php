d9b9d64a629dcbdc22922c75dca2ccb09383bd4b
dayAlone <andrey.slider@gmail.com>
2015-06-15 11:51:31 +0500

    Changes

--- public_html/local/templates/.default/components/bitrix/news.list/news/template.php
+++ public_html/local/templates/.default/components/bitrix/news.list/news/template.php
@@ -3,7 +3,7 @@
 foreach ($arResult['ITEMS'] as $item):?>
   <div class="news-list__item">
   	<span class="news-list__frame">
-	    <span class="news-list__date"><?=(LANGUAGE_ID=='ru'?r_date($item['ACTIVE_FROM']):date('d.m.Y', $item['ACTIVE_FROM']))?></span>
+	    <span class="news-list__date"><?=(LANGUAGE_ID=='ru'?r_date($item['ACTIVE_FROM']):date('d.m.Y', strtotime($item['ACTIVE_FROM'])))?></span>
 	    <span class="news-list__section"><?=$arResult['SECTIONS']['PATH'][$item['IBLOCK_SECTION_ID']]?></span>
 	</span>
     <div class="news-list__divider"></div>

2568f2143a002d2ff0653afb65344fa6b52ae82c
dayAlone <andrey.slider@gmail.com>
2015-06-15 15:13:55 +0500

    Changes

--- public_html/en/contacts/index.php
+++ public_html/en/contacts/index.php
@@ -13,6 +13,6 @@ $APPLICATION->SetPageProperty('body_class', "contacts");?><div class="page__cont
 	</div>
 </div>
 <div id="map" data-coords="55.72290917,37.56046120" class="xl-margin-top l-margin-bottom"></div>
-<script src="http://api-maps.yandex.ru/2.1/?lang=ru-RU&amp;load=package.full" type="text/javascript"></script><?
+<script src="http://api-maps.yandex.ru/2.1/?lang=en-US&amp;load=package.full" type="text/javascript"></script><?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>

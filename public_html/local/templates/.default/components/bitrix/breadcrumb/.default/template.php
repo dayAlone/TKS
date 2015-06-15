6f0fa07b5b28ca1a3dbe5dda85ffee473129bbd8
dayAlone <andrey.slider@gmail.com>
2015-06-15 15:04:58 +0500

    Changes

--- public_html/local/templates/.default/components/bitrix/breadcrumb/.default/template.php
+++ public_html/local/templates/.default/components/bitrix/breadcrumb/.default/template.php
@@ -4,16 +4,19 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 //delayed function must return a string
 if(empty($arResult))
 	return "";
-
+$elements = array();
 $num_items = count($arResult);
 for($index = 0, $itemSize = $num_items; $index < $itemSize; $index++)
 {
 	$title = htmlspecialcharsex($arResult[$index]["TITLE"]);
-	
-	if($arResult[$index]["LINK"] <> "" && $index != $itemSize-1)
-		$strReturn .= '<a href="'.$arResult[$index]["LINK"].'" title="'.$title.'">'.$title.'</a><span>></span>';
-	else
-		$strReturn .= '<span>'.$title.'</span></li>';
+	if(!in_array($title, $elements)):
+		if($arResult[$index]["LINK"] <> "" && $index != $itemSize-1)
+			$strReturn .= '<a href="'.$arResult[$index]["LINK"].'" title="'.$title.'">'.$title.'</a><span>></span>';
+		else
+			$strReturn .= '<span>'.$title.'</span></li>';
+
+		$elements[] = $title;
+	endif;
 }
 
 return $strReturn;

4bfb8345ee4ab1ae5e6598781a36ddd2a7fef24b
dayAlone <andrey.slider@gmail.com>
2015-06-18 17:10:14 +0500

    Changes

--- public_html/local/templates/.default/components/bitrix/news.detail/career/template.php
+++ public_html/local/templates/.default/components/bitrix/news.detail/career/template.php
@@ -20,5 +20,5 @@ $item = $arResult;
 	<a href="/career/" class="career-item__back"><?=svg('back')?> <?=GetMessage('BACK')?></a>
 </div>
 <?$this->SetViewTarget('footer');
-require($_SERVER['DOCUMENT_ROOT'].'/include/vancancy-'.LANGUAGE_ID.'.php');
+require($_SERVER['DOCUMENTS_ROOT'].'/include/vancancy-'.LANGUAGE_ID.'.php');
 $this->EndViewTarget();?> 

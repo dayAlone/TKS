854dfa0dc5973c8988f5341775b978ee4ae1c98d
dayAlone <andrey.slider@gmail.com>
2015-06-19 17:10:48 +0500

    Changes

--- public_html/en/industries/.left.menu.php
+++ public_html/en/industries/.left.menu.php
@@ -6,7 +6,7 @@ $aMenuLinks = $APPLICATION->IncludeComponent("bitrix:menu.sections","",Array(
         "SECTION_PAGE_URL" => "#SECTION_CODE#/", 
         "DETAIL_PAGE_URL" => "#ELEMENT_CODE#/", 
         "IBLOCK_TYPE" => "news", 
-        "IBLOCK_ID" => "7", 
+        "IBLOCK_ID" => 13, 
         "DEPTH_LEVEL" => "2", 
         "CACHE_TYPE" => "A", 
     )
--- public_html/en/map/index.php
+++ public_html/en/map/index.php
@@ -1,6 +1,6 @@
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
-$APPLICATION->SetTitle('Карта сайта');
+$APPLICATION->SetTitle('Sitemap');
 $APPLICATION->IncludeComponent(
   "bitrix:main.map", 
   ".default", 

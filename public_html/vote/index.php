190a3d364e7b8e15d47ac6f664c276b4813deef5
dayAlone <andrey.slider@gmail.com>
2015-06-19 18:14:29 +0500

    Changes

--- public_html/vote/index.php
+++ public_html/vote/index.php
@@ -4,7 +4,7 @@ $APPLICATION->SetTitle("Опрос");
 $APPLICATION->SetPageProperty('body_class', "contacts");?><div class="page__content">
 <?$APPLICATION->IncludeComponent(
 	"bitrix:voting.form", 
-	"", 
+	"vote", 
 	array(
 		"VOTE_ID" => "1",
 		"VOTE_RESULT_TEMPLATE" => "/vote/",
